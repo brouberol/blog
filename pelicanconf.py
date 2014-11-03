222e2ebf5f023c9ad1b521ef9e6953bc6ed0a8c4
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2014-11-03 23:50:11 +0100

    Add RSS feeds
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import platform
 from os.path import abspath, join, dirname
-
 AUTHOR = u'Balthazar Rouberol'
 SITENAME = u'🐵 - Edimbara'
-SITEURL = 'http://blog.brl.ovh'
+if platform.node() == 'linux-pbat.site':
+    SITEURL = ''
+else:
+    SITEURL = 'http://blog.brl.ovh'
 
 PATH = 'content'
 
@@ -14,8 +17,8 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'fr'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_RSS = 'feeds/rss.xml'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 TRANSLATION_FEED_ATOM = None
 STATIC_PATHS = ['images', ]
 THEME = abspath(join(dirname(__file__), 'themes', 'aboutwilson'))
