b3fa22e8e6e2f35aa0d5535ba9bbccf57b6ccc1c
Balthazar Rouberol <br@imap.cc>
2019-06-08 10:16:52 +0200

    Config reorg

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-import platform
 from os.path import abspath, join, dirname
+
 AUTHOR = u'Balthazar Rouberol'
 SITENAME = u'Balthazar'
-if platform.node() in ['morenika', 'Balthazars-MacBook-Pro.local']:
-    SITEURL = 'http://localhost:8000'
-else:
-    SITEURL = 'https://blog.balthazar-rouberol.com'
-
-PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
-
-DEFAULT_LANG = u"en"
+DEFAULT_LANG = "en"
 LOCALE = 'en_US.utf-8'
 
 # Feed generation is usually not desired when developing
@@ -22,8 +15,11 @@ FEED_RSS = 'feeds/rss.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 TRANSLATION_FEED_ATOM = None
 RSS_FEED_SUMMARY_ONLY = False
+
+PATH = 'content'
 STATIC_PATHS = ['images', ]
 THEME = abspath(join(dirname(__file__), 'themes', 'pelican-hyde'))
+
 PROFILE_IMAGE = 'https://balthazar-rouberol.com/static/img/image-small.jpg'
 BIO = 'I work with humans and computers.'
 
@@ -38,11 +34,10 @@ SOCIAL = [
     ('linkedin', 'https://www.linkedin.com/in/balthazar-rouberol/'),
 ]
 
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 DISQUS_SITENAME = 'balthazar-blog'
 PLUGIN_PATHS = ["pelican-plugins"]
