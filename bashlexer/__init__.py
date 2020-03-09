c725ca7cb891706a51782a88872caca9efa19880
Balthazar Rouberol <balthazar.rouberol@datadoghq.com>
2020-03-09 21:13:21 +0100

    Add custom bash lexer

---
+++ bashlexer/__init__.py
@@ -0,0 +1,24 @@
+from pygments.lexers.shell import BashLexer
+from pygments.token import Keyword, Token
+
+
+class ExtendedBashLexer(BashLexer):
+    name = 'extbash'
+    aliases = ['extbash', 'extzsh', 'extconsole', 'extshell']
+
+    EXTRA_BUILTINS = [
+        'alias', 'which', 'typeset', 'echo', 'cat', 'ls', 'mkdir',
+        'cp', 'mv', 'rm', 'man', 'ln', 'grep', 'less', 'awk', 'sed',
+        'cut', 'paste', 'head', 'tail', 'wc', 'sort', 'uniq', 'tr',
+        'fold'
+    ]
+    EXTRA_KEYWORDS = ['>', '<', '<<', '>>', '&', '|']
+
+    def get_tokens_unprocessed(self, text):
+        for index, token, value in BashLexer.get_tokens_unprocessed(self, text):
+            if value in self.EXTRA_KEYWORDS:
+                yield index, Keyword, value
+            elif value in self.EXTRA_BUILTINS:
+                yield index, Token.Name.Builtin, value
+            else:
+                yield index, token, value
