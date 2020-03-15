6f67b31a11f153295394713f12a728c4958756e6
Balthazar Rouberol <balthazar.rouberol@datadoghq.com>
2020-03-15 11:11:10 +0100

    Hack with author url

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,7 @@ from os.path import abspath, join, dirname
 
 AUTHOR = "Balthazar Rouberol"
 AUTHOR_TWITTER = "@brouberol"
+AUTHOR_URL = "https://balthazar-rouberol.com"
 SITENAME = "Balthazar"
 SITEURL = "https://blog.balthazar-rouberol.com"
 ISSOURL = "https://comments.balthazar-rouberol.com"
