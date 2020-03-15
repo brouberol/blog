0add6256633829d8c93fd7f2d2b474183562e3b5
Balthazar Rouberol <balthazar.rouberol@datadoghq.com>
2020-03-15 11:16:59 +0100

    Improve locale date format

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,7 @@ ABSOLUTE_SITEURL = SITEURL
 TIMEZONE = "Europe/Paris"
 DEFAULT_LANG = "en"
 LOCALE = "en_US.utf-8"
+DEFAULT_DATE_FORMAT = "%b %d, %Y"
 
 # Feed generation is usually not desired when developing
 FEED_RSS = "feeds/rss.xml"
