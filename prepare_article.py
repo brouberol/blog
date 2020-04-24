1c3f0b6d9e4f74184a556f5902cadd02f68816b8
Balthazar Rouberol <br@imap.cc>
2020-04-24 11:54:38 +0200

    Improve settex pattern to leave the chapter metadata out

--- prepare_article.py
+++ prepare_article.py
@@ -5,26 +5,26 @@ import re
 
 filename = sys.argv[1]
 
-LINK_PATTERN = r'!\[([^\]]+)\]\((.+)\)\n\n'
-REPLACE_LINK_BY = '![\\1](\\2)\n<span class=imgcaption>\\1</span>\n\n'
-SETEXT_H1_PATTERN = r'([\w\' ]+)\n(=){2,}'
-ATX_H1_PATTERN = '# \\1'
-SETEXT_H2_PATTERN = r'([^\n]+)\n(-){2,}'
-ATX_H2_PATTERN = '## \\1'
+LINK_PATTERN = r"!\[([^\]]+)\]\((.+)\)\n\n"
+REPLACE_LINK_BY = "![\\1](\\2)\n<span class=imgcaption>\\1</span>\n\n"
+SETEXT_H1_PATTERN = r"([\w\' ]+)\n(=){2,}"
+ATX_H1_PATTERN = "# \\1"
+SETEXT_H2_PATTERN = r"([^\n:]+)\n(-){2,}"
+ATX_H2_PATTERN = "## \\1"
 TITLE_META_PATTERN = r"Title:\n"
-LABEL_PATTERN = r'`\\label\{.+}`\{=tex\}'
-SHELL_BLOCKQUOTE_PATTERN = r'``` *(console|shell|zsh|bash)'
-SHELL_BLOCKQUOTE_REPLACE = '``` ext\\1'
+LABEL_PATTERN = r"`\\label\{.+}`\{=tex\}"
+SHELL_BLOCKQUOTE_PATTERN = r"``` *(console|shell|zsh|bash)"
+SHELL_BLOCKQUOTE_REPLACE = "``` ext\\1"
 
 
 with open(filename) as f:
     data = f.read()
 
-    data = re.sub(r'’', "'", data)
+    data = re.sub(r"’", "'", data)
 
     # Collapse multiline md image links
     for match in re.findall(LINK_PATTERN, data):
-        data = data.replace(match[0], match[0].replace('\n', ' '))
+        data = data.replace(match[0], match[0].replace("\n", " "))
 
     # Insert image captions
     data = re.sub(LINK_PATTERN, REPLACE_LINK_BY, data)
@@ -32,7 +32,7 @@ with open(filename) as f:
     m = re.search(SETEXT_H1_PATTERN, data)
     if m:
         title = m.group(1)
-        data = re.sub(TITLE_META_PATTERN, "Title: " + title + '\n', data)
+        data = re.sub(TITLE_META_PATTERN, "Title: " + title + "\n", data)
 
     # Replace h1 and h2 settext styles by ATX styles
     data = re.sub(SETEXT_H1_PATTERN, ATX_H1_PATTERN, data)
@@ -42,11 +42,11 @@ with open(filename) as f:
     data = data.replace("Aspiring Software\nDeveloper", "Aspiring Software Developer")
 
     # Remove latex labels
-    data = re.sub(LABEL_PATTERN, '', data)
+    data = re.sub(LABEL_PATTERN, "", data)
 
     data = re.sub(SHELL_BLOCKQUOTE_PATTERN, SHELL_BLOCKQUOTE_REPLACE, data)
 
     data = data.replace("## Exercises", "## Going further")
 
-with open(filename, 'w') as out:
+with open(filename, "w") as out:
     out.write(data)
