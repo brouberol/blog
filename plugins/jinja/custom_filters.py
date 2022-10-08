4993407e994934f5f5752f395d63392279fadd8c
Balthazar Rouberol <br@imap.cc>
2022-10-08 10:15:38 +0200

    Capitalize single word tags as well

--- plugins/jinja/custom_filters.py
+++ plugins/jinja/custom_filters.py
@@ -1,8 +1,8 @@
-from pelican.contents import Category
+from pelican.contents import Category, Tag
 
 
 def single_word_capitalize(s):
     if len(str(s).split()) == 1:
-        if isinstance(s, Category):
+        if isinstance(s, (Category, Tag)):
             s.name = s.name.capitalize()
     return s
