801c11107c6051d3d13bc8b69f7bcc868bf2f5bf
Balthazar Rouberol <br@imap.cc>
2023-05-03 16:23:26 +0200

    Don't capitalize fully uppercase tags

--- plugins/jinja/custom_filters.py
+++ plugins/jinja/custom_filters.py
@@ -4,5 +4,6 @@ from pelican.contents import Category, Tag
 def single_word_capitalize(s):
     if len(str(s).split()) == 1:
         if isinstance(s, (Category, Tag)):
-            s.name = s.name.capitalize()
+            if not s.name.isupper():
+                s.name = s.name.capitalize()
     return s
